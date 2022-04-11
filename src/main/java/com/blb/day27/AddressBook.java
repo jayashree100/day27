fee1a1e30cab24d8dd347aa8ebcb5327534e1c7f
JAISHRI <sjayashree529@gmail.com>
2022-04-11 16:23:02 +0530

    changed

---
+++ src/main/java/com/blb/day27/AddressBook.java
@@ -0,0 +1,10 @@
+package com.blb.day27;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBook {
+
+	public static Map<String, Contacts> book = new HashMap<String, Contacts>();
+
+}
---
+++ src/test/java/com/bridge/day27/AddressBookMainTest.java
@@ -0,0 +1,12 @@
+package com.bridge.day27;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		System.out.println("File I/O");
+		assertTrue(true);
+	}
+}
